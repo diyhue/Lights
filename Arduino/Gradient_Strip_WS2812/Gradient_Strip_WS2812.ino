c3c37c4a66c4513a05dc2dabc837c68dc1698b03
Motea Marius <marius.motea@gmail.com>
2021-10-07 21:25:35 +0300

    Fix startup lights count restore

--- Arduino/Gradient_Strip_WS2812/Gradient_Strip_WS2812.ino
+++ Arduino/Gradient_Strip_WS2812/Gradient_Strip_WS2812.ino
@@ -575,6 +575,7 @@ void setup() {
 
 
   ChangeNeoPixels(pixelCount);
+  lightLedsCount = pixelCount / (lightsCount - 1);
 
   if (startup == 1) {
     for (uint8_t i = 0; i < lightsCount; i++) {
@@ -755,7 +756,6 @@ void setup() {
     sprintf(macString, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     DynamicJsonDocument root(1024);
     root["name"] = lightName;
-    root["lights"] = 1;
     root["protocol"] = "native_single";
     root["modelid"] = "LCX002";
     root["type"] = "ws2812_gradient_strip";
